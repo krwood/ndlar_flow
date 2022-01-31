1f971a81e2dd80f44ee80ba0073af30578c8fc8e
Kevin Wood <kevin.wood@stonybrook.edu>
2022-01-31 15:20:34 -0800

    tidies up repo for ndlar adaptation

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ with open('README.rst', 'r', encoding='utf-8') as fh:
 with open('VERSION', 'r') as fh:
     version = fh.read().strip()
 
-setuptools.setup(name='module0_flow',
+setuptools.setup(name='ndlar_flow',
                  version=version,
-                 description='An h5flow-based analysis framework for Module0 data',
+                 description='An h5flow-based analysis framework for (multi-module) ndlar simulation.',
                  long_description=long_description,
                  long_description_content_type='text/x-rst',
-                 author='Peter Madigan',
-                 author_email='pmadigan@berkeley.edu',
-                 package_dir={'module0_flow': './h5flow_modules/'},
-                 packages=[p.replace('h5flow_modules', 'module0_flow') for p in setuptools.find_packages(where='.')],
+                 author='Peter Madigan, Stephen Greenberg',
+                 author_email='pmadigan@berkeley.edu, kwood@lbl.gov',
+                 package_dir={'ndlar_flow': './h5flow_modules/'},
+                 packages=[p.replace('h5flow_modules', 'ndlar_flow') for p in setuptools.find_packages(where='.')],
                  python_requires='>=3.7',
                  install_requires=[
                      'h5py>=2.10',
